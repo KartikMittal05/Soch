a268a6a55b1f8875514288b2d394ddb03721a45b
Kartik Mittal <118724619+KartikMittal05@users.noreply.github.com>
2024-01-10 00:45:59 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+include("dbconnect.php");
+?>
+<?php
+$un=$_POST['t1'];
+$pd=$_POST['t2'];
+$c=0;
+$sql="SELECT * FROM userdetails";
+$result=mysqli_query($conn,$sql);
+    while($row=mysqli_fetch_assoc($result))
+    {
+        if($row["username"]==$un && $row["password"]==$pd)
+        $c=1;
+        header("location:try2.html");
+    }
+    if($c==0){
+        echo "Incorrect username or Password";
+    }
+    mysqli_close($conn);
+    ?>
